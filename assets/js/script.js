6bc3fa110bf1c73887979de3848f71fe09a94d7b
Lilla Kavecsanszki <lilla-kavecsanszki@outlook.com>
2023-01-20 17:54:48 +0000

    Add Javascript to start the quiz, get questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ let currentQuestion = {}
 let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
-let availableQuestion = [];
+let availableQuestions = [];
 
 let questions = [
     {
@@ -91,4 +91,66 @@ let questions = [
         choice4: 'lily',
         answer:
     },
-]
+];
+
+const SCORE_POINTS = 10;
+const MAX_QUESTIONS = 10;
+
+startGame () {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+}
+
+getNewQuestion() {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+    questionCounter ++;
+    progressText.innerHTML = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(question/MAX_QUESTIONS)*100}%`;
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionsIndex];
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number]
+    })  
+
+    availableQuestions.splice(questionsIndex, 1);
+    acceptingAnswers === true;
+}
+
+choices.forEach(choice {
+    choice.addEventListener('click', e {
+        if(!acceptingAnswers) 
+        return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        let classToApply = selectedAnswer === currentQuestion.answer ? 'correctanswer' : 'wronganswer';
+
+        if(classToApply === 'correctanswer') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeOut(() {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+    })
+})
+
+incrementScore = num {
+    score += num;
+    scoreText.innerText = score;
+}
